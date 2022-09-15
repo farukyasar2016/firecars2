6143bdf7e44762bafaabbfae6aa52db2fd7b0158
farukyasar2016 <113189026+farukyasar2016@users.noreply.github.com>
2022-09-15 15:37:25 +0200

    Add files via upload

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firecars/shared-ui/carList.dart';
 import 'package:firecars/views/home/homeAppBar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import 'addCarSection.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -13,7 +16,9 @@ class Home extends StatelessWidget {
       body: SafeArea(
         child: CustomScrollView(
           slivers: [
-            HomeAppBar(user : _user)
+            HomeAppBar(user : _user),
+            AddCarSection(user: _user),
+            CarList(userID: _user!.uid),
           ],
         ),
       )
